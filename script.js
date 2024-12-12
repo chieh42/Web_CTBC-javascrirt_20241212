72d2a8a8ae251153344eae47e626b88117f23807
chieh42 <ss2018019@gmail.com>
2024-12-12 13:08:46 +0800

    Update 建立基本架構

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('這是外部的JavaScript');
+
+// 變數variable
+// 儲存網站內的資料 例如 顏色 字體大小 元素 使用者資料 等等
+
+// 變數語法
+// let 變數名稱
+
+// 變數名稱命名習慣(守則)
+// 1. 不能使用數字開頭
+// 2. 不能使用空格 test 1
+// 3. 不能使用全形符號 @
+// 4. 不能使用中文命名
+// 5. 保留字(有作用的關鍵字)if let
+
+// 建議命名方式(駝峰式命名法)
+// 多個單字構成
+// 開頭使用小寫
+// 單字相連字首大寫
+// 用有意義的名稱命名
+
+let test=42;
+let fontColor='#f63';
+
+console.log(test);
+console.log(fontColor);
+
+test=47;
+console.log(test);
