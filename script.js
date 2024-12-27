2d1ea28ed09ba8258877225d8f41ac0d7a67b873
chieh42 <ss2018019@gmail.com>
2024-12-27 13:35:34 +0800

    Updata 認識函式

--- script.js
+++ script.js
@@ -174,3 +174,115 @@ logLine();
 logLine();
 logLine();
 logLine();
+
+// 對照組(比較不適合的寫法)
+function logLineRed(){
+    console.log("%c---我是分隔線---","color:red");
+}
+
+function logLineGreen(){
+    console.log("%c---我是分隔線---","color:green");
+}
+
+logLineRed();
+logLineGreen();
+
+// 城市要注意的事情 思考未來的維護性 擴充性 可讀性
+
+// 比較適合的寫法
+// function 函式名稱(參數1,參數2,...){
+function logLineWithColor(color){
+    console.log("%c---我是分隔線---",`color:${color}`);
+}
+
+logLineWithColor("red");
+logLineWithColor("green");
+logLineWithColor("blue");
+
+// 參數的預設值
+function logLineWithColor(color="black"){
+    console.log("%c---我是分隔線---",`color:${color}`);
+}
+
+logLineWithColor("red");
+// 不給參數會直接使用預設值
+logLineWithColor();
+
+console.log("%c表達式函式","color:gold");
+
+// 表達式函式語法
+// 會比較常使用
+const print =function(color="red"){
+    console.log("%c測試",`color:${color}`);
+}
+
+// 變數名稱(參數)
+print("green");
+print("gray");
+
+const printMessage=function(color="red",message="這是預設文字"){
+    console.log(`%c${message}`,`color:${color}`);
+}
+
+printMessage();
+printMessage("green");
+printMessage("green","這是測試文字");
+// 想要使用預設顏色 文字要使用"訊息"index.html
+printMessage("這是測試文字");                 //錯誤
+printMessage(undefined,"這是測試文字");       //正確結果
+
+console.log("%c函式回傳值","color:gold");
+
+// 有傳回值的函式 必須使用一個return關鍵字將值傳回
+function double(number){
+    return number*2;
+}
+
+// 使用方法一 用變數將傳回值儲存
+const result=double(10);
+console.log(result);
+
+// 使用方法二 直接將傳回函式當作數值使用
+console.log(double(7));
+
+// 表達式傳回函式
+// 平方函式
+const square=function(number){
+    return number*number;
+}
+
+console.log(square(5));
+
+// 提升hosting
+// 在定義函數以前就能夠呼叫
+// 一般函式會提升
+console.log("一般函式:",Math.floor(BMI(60,1.68)));
+// 表達式函式不會提升 不能在定義以前呼叫(相對安全)
+// console.log("表達式函式:",Math.floor(BmiExpression(60,1.68)));
+// console.log("箭頭函式:",Math.floor(bmiArrow(60,1.68)));
+
+
+// 練習題
+// BMI=體重/身高^2
+// 一般函式
+function BMI(weight,height){
+    return weight/square(height);
+}
+console.log(BMI(60,1.68));
+console.log(BMI(70,1.83));
+
+// 表達函式
+const BmiExpression=function(weight,height){
+    return weight/square(height);
+}
+// Math.floor可以去掉小數點
+console.log(Math.floor(BmiExpression(60,1.68)));
+console.log(Math.floor(BmiExpression(70,1.83)));
+
+// 箭頭函式 =>
+const bmiArrow=(weight,height)=>{
+    return weight/square(height);
+}
+
+console.log(Math.floor(bmiArrow(60,1.68)));
+console.log(Math.floor(bmiArrow(70,1.83)));
